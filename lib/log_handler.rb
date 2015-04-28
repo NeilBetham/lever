94d46deab74f9086c867d51bab48cbc4f23b1474
Neil Betham <nbetham@me.com>
2015-04-28 12:13:27 -0400

    probably fix log handler

--- lib/log_handler.rb
+++ lib/log_handler.rb
@@ -2,7 +2,7 @@ module Lever
   # LogHandler's sole purpose is to sequentially handle parts of logs so index values remain consistent
   class LogHandler
     def initialize(log)
-      @part_queue = EventMachine::Queue.new
+      @part_queue = EventMachine::Channel.new
       @processing = false
       @log = Log.find log.id
       @sync_defer = nil
@@ -15,19 +15,22 @@ module Lever
       @processing = true
     end
 
-    # Sequential part handler
+    # Sequential part handler, handles 5 parts
     def handle_parts
       (0..4).each do
-        @part_queue.pop do |part|
-          if part
-            @log.add_part part
-          else
-            break
-          end
+        part = @part_queue.pop
+
+        if part
+          @log.add_part part
+        else
+          @processing = false
+          @sync_defer.succeed if @sync_defer
+          return
         end
       end
-      @sync_defer.succeed if @sync_defer && @part_queue.num_waiting == 0
-      @part_queue.num_waiting > 0 ? EM.next_tick { EM.synchrony { handle_parts } } : @processing = false
+
+      # Loop fell through so there are probably still parts left
+      EM.next_tick { EM.synchrony { handle_parts } }
     end
 
     # Call when waiting for queue to empty
